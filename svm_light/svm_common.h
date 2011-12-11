1d549bdfb46fd027e97f09b7ed4772a23f42f9e9
unknown <Administrator@.(none)>
2011-12-11 14:27:03 +0000

    windows: workaround for missing stdint.h header

--- svm_light/svm_common.h
+++ svm_light/svm_common.h
@@ -85,7 +85,16 @@ MexPhiCustomGetLabel (MexPhiCustom phi) {
 }
 
 # include <stdio.h>
-# include <stdint.h>
+
+#ifdef _MSC_VER
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
+#include <stdint.h>
+#endif
+
 # include <ctype.h>
 # include <math.h>
 # include <string.h>
